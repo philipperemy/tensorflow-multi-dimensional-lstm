a0028d031ab1301e0339b4b400036828a65df6e6
philipperemy <premy@reactive.co.jp>
2017-06-15 17:46:05 +0900

    WIP

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import sys
 from time import time
 
 import numpy as np
+import tensorflow.contrib.slim as slim
+from tensorflow.python.ops.rnn import dynamic_rnn
 
+from data_gen import next_batch
 from md_lstm import *
 
 
@@ -16,43 +20,73 @@ def create_bias_variable(name, shape):
     return tf.Variable(initializer(shape=shape), name)
 
 
-def run():
-    learning_rate = 0.001
+def multi_dimensional_lstm(input_data, rnn_size):
+    rnn_out, _ = multi_dimensional_rnn_while_loop(rnn_size=rnn_size, input_data=input_data, sh=[1, 1])
+    return rnn_out
+
+
+def standard_lstm(input_data, rnn_size):
+    b, h, w, c = input_data.get_shape().as_list()
+    new_input_data = tf.reshape(input_data, (b, h * w, c))
+    rnn_out, _ = dynamic_rnn(tf.contrib.rnn.LSTMCell(rnn_size),
+                             inputs=new_input_data,
+                             dtype=tf.float32)
+    rnn_out = tf.reshape(rnn_out, (b, h, w, rnn_size))
+    return rnn_out
+
+
+def run(m_id):
+    use_multi_dimensional_lstm = (m_id == 1)
+
+    anisotropy = False
+    learning_rate = 0.001 * 0.5
     batch_size = 16
     h = 32
     w = 32
-    channels = 4
+    channels = 1
     x = tf.placeholder(tf.float32, [batch_size, h, w, channels])
-    y = tf.placeholder(tf.float32, [batch_size, 10])
+    y = tf.placeholder(tf.float32, [batch_size, h, w, channels])
 
-    def next_batch():
-        return np.random.uniform(size=(batch_size, h, w, channels)), np.random.randint(low=0, high=2,
-                                                                                       size=(batch_size, 10))
+    hidden_size = 4
+    if use_multi_dimensional_lstm:
+        print('Using Multi Dimensional LSTM !')
+        rnn_out, _ = multi_dimensional_rnn_while_loop(rnn_size=hidden_size, input_data=x, sh=[1, 1])
+    else:
+        print('Using Standard LSTM !')
+        rnn_out = standard_lstm(input_data=x, rnn_size=hidden_size)
 
-    hidden_size = 64
-    outputs, _ = multi_dimensional_rnn_while_loop(rnn_size=hidden_size, input_data=x, sh=[2, 2])
-    rnn_out = tf.squeeze(outputs[:, -1, -1, :])
+    model_out = slim.fully_connected(inputs=rnn_out,
+                                     num_outputs=1,
+                                     activation_fn=None)
 
-    fc0_w = create_weight_variable('fc0_w', [hidden_size, 10])
-    fc0_b = create_bias_variable('fc0_b', [10])
-    y = tf.matmul(rnn_out, fc0_w) + fc0_b
-
-    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y))
-    grad_update = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
-    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    loss = 1e4 * tf.reduce_mean(tf.abs(tf.subtract(y, model_out)))
+    grad_update = tf.train.AdamOptimizer(learning_rate).minimize(loss)
 
     sess = tf.Session(config=tf.ConfigProto(log_device_placement=False))
     sess.run(tf.global_variables_initializer())
 
-    steps = 100
+    steps = 100000
     for i in range(steps):
-        batch = next_batch()
+        batch = next_batch(batch_size, h, w, anisotropy)
         st = time()
-        tr_loss, tr_acc, _ = sess.run([cross_entropy, accuracy, grad_update],
-                                      feed_dict={x: batch[0], y: batch[1]})
-        print('Forward-Backward pass took {0:.2f}s to complete.'.format(time() - st))
+        batch_x = np.expand_dims(batch[0], axis=3)
+        batch_y = np.expand_dims(batch[1], axis=3)
+
+        if not use_multi_dimensional_lstm and i == 0:
+            print('Shuffling the batch in the height dimension for the standard LSTM.'
+                  'Its like having h LSTM on the width axis.')
+            perms = np.random.permutation(list(range(w)))
+            batch_x = batch_x[:, perms, :, :]
+            batch_y = batch_y[:, perms, :, :]
+            pass
+
+        loss_val, _ = sess.run([loss, grad_update], feed_dict={x: batch_x, y: batch_y})
+        print('steps = {0} | loss = {1:.3f} | time {2:.3f}'.format(str(i).zfill(3),
+                                                                   loss_val,
+                                                                   time() - st))
 
 
 if __name__ == '__main__':
-    run()
+    assert len(sys.argv) == 2, 'Please specify Model 0: LSTM, 1: MD LSTM'
+    model_id = int(sys.argv[1])
+    run(model_id)
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
+#!/usr/bin/env bash
 rm out.MD_LSTM.txt
 rm out.SI_LSTM.txt
-export CUDA_VISIBLE_DEVICES=; nohup python3 -u main_2.py 0 > out.SI_LSTM.txt 2>&1 &
-export CUDA_VISIBLE_DEVICES=; nohup python3 -u main_2.py 1 > out.MD_LSTM.txt 2>&1 &
+export CUDA_VISIBLE_DEVICES=; nohup python3 -u main.py 0 > out.SI_LSTM.txt 2>&1 &
+export CUDA_VISIBLE_DEVICES=; nohup python3 -u main.py 1 > out.MD_LSTM.txt 2>&1 &
