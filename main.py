37d8719a0498d159bc21a24a0dba584b2cbf9cb3
philipperemy <premy@cogent.co.jp>
2018-05-17 22:16:58 +0900

    update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 import tensorflow.contrib.slim as slim
 from tensorflow.python.ops.rnn import dynamic_rnn
 
-from data_gen_corr_fields import next_batch, visualise_mat
+from data_gen_corr_fields import next_batch, visualise_mat, find_target_for_tensor
 from md_lstm import *
 
 
@@ -13,7 +13,7 @@ def standard_lstm(input_data, rnn_size):
     # input is (b, h, w, c)
     b, h, w, c = input_data.get_shape().as_list()
     # transpose = swap h and w.
-    new_input_data = tf.reshape(input_data, (b * w, h, c))  # vertical
+    new_input_data = tf.reshape(input_data, (b * h, w, c))  # vertical
     rnn_out, _ = dynamic_rnn(tf.contrib.rnn.LSTMCell(rnn_size),
                              inputs=new_input_data,
                              dtype=tf.float32)
@@ -24,7 +24,7 @@ def standard_lstm(input_data, rnn_size):
 def run(m_id):
     use_multi_dimensional_lstm = (m_id == 1)
 
-    learning_rate = 0.001
+    learning_rate = 0.01
     batch_size = 16
     h = 8
     w = 8
@@ -32,7 +32,7 @@ def run(m_id):
     x = tf.placeholder(tf.float32, [batch_size, h, w, channels])
     y = tf.placeholder(tf.float32, [batch_size, h, w, channels])
 
-    hidden_size = 8
+    hidden_size = 16
     if use_multi_dimensional_lstm:
         print('Using Multi Dimensional LSTM!')
         rnn_out, _ = multi_dimensional_rnn_while_loop(rnn_size=hidden_size, input_data=x, sh=[1, 1])
@@ -57,22 +57,17 @@ def run(m_id):
         batch_x = np.expand_dims(batch[0], axis=3)
         batch_y = np.expand_dims(batch[1], axis=3)
 
-        if not use_multi_dimensional_lstm and i == 0:
-            print('Shuffling the batch in the height dimension for the standard LSTM.'
-                  'Its like having h LSTM on the width axis.')
-            perms = np.random.permutation(list(range(w)))
-            batch_x = batch_x[:, perms, :, :]
-            batch_y = batch_y[:, perms, :, :]
-
         mo, loss_val, _ = sess.run([model_out, loss, grad_update], feed_dict={x: batch_x,
                                                                               y: batch_y})
 
-        rl = np.mean(np.square(mo[np.where(batch_y == 1)] - batch_y[np.where(batch_y == 1)]))
+        ys = np.array([batch_y[i, x, y, 0] for (i, (y, x)) in enumerate(find_target_for_tensor(batch_y))])
+        mos = np.array([mo[i, x, y, 0] for (i, (y, x)) in enumerate(find_target_for_tensor(batch_y))])
+        rl = np.mean(np.square(ys - mos))
         print('steps = {0} | loss = {1:.3f} | time {2:.3f} | rl = {3:.3f}'.format(str(i).zfill(3),
                                                                                   loss_val,
                                                                                   time() - st, rl))
 
-        if i % 50 == 0:
+        if i % 5000 == 0:
             visualise_mat(sess.run(model_out, feed_dict={x: batch_x})[0].squeeze())
             visualise_mat(batch_y[0].squeeze())
 
