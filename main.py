6aef7f3ac7846b0a36c6efe499b62c09e283208c
philipperemy <premy@cogent.co.jp>
2018-05-17 21:44:18 +0900

    update

--- main.py
+++ main.py
@@ -5,19 +5,15 @@ import numpy as np
 import tensorflow.contrib.slim as slim
 from tensorflow.python.ops.rnn import dynamic_rnn
 
-from data_gen import next_batch
+from data_gen_corr_fields import next_batch, visualise_mat
 from md_lstm import *
 
 
-# Anisotropy: vertical or horizontal
-# - Having a LSTM vertical or a LSTM horizontal is sufficient.
-
-# Isotropy: random gaussian fields have terms that depend on all the directions (not only horizontal or vertical)
-# - MD LSTM on all four directions should help there.
-
 def standard_lstm(input_data, rnn_size):
+    # input is (b, h, w, c)
     b, h, w, c = input_data.get_shape().as_list()
-    new_input_data = tf.reshape(input_data, (b, h * w, c))
+    # transpose = swap h and w.
+    new_input_data = tf.reshape(input_data, (b * w, h, c))  # vertical
     rnn_out, _ = dynamic_rnn(tf.contrib.rnn.LSTMCell(rnn_size),
                              inputs=new_input_data,
                              dtype=tf.float32)
@@ -28,16 +24,15 @@ def standard_lstm(input_data, rnn_size):
 def run(m_id):
     use_multi_dimensional_lstm = (m_id == 1)
 
-    anisotropy = False
     learning_rate = 0.001
     batch_size = 16
-    h = 32
-    w = 32
+    h = 8
+    w = 8
     channels = 1
     x = tf.placeholder(tf.float32, [batch_size, h, w, channels])
     y = tf.placeholder(tf.float32, [batch_size, h, w, channels])
 
-    hidden_size = 32
+    hidden_size = 8
     if use_multi_dimensional_lstm:
         print('Using Multi Dimensional LSTM!')
         rnn_out, _ = multi_dimensional_rnn_while_loop(rnn_size=hidden_size, input_data=x, sh=[1, 1])
@@ -47,9 +42,9 @@ def run(m_id):
 
     model_out = slim.fully_connected(inputs=rnn_out,
                                      num_outputs=1,
-                                     activation_fn=None)
+                                     activation_fn=tf.nn.sigmoid)
 
-    loss = 1e4 * tf.reduce_mean(tf.abs(tf.subtract(y, model_out)))
+    loss = tf.reduce_mean(tf.square(y - model_out))
     grad_update = tf.train.AdamOptimizer(learning_rate).minimize(loss)
 
     sess = tf.Session(config=tf.ConfigProto(log_device_placement=False))
@@ -57,10 +52,11 @@ def run(m_id):
 
     steps = 100000
     for i in range(steps):
-        batch = next_batch(batch_size, h, w, anisotropy)
+        batch = next_batch(batch_size, h, w)
         st = time()
         batch_x = np.expand_dims(batch[0], axis=3)
         batch_y = np.expand_dims(batch[1], axis=3)
+        batch_t = batch[2]
 
         if not use_multi_dimensional_lstm and i == 0:
             print('Shuffling the batch in the height dimension for the standard LSTM.'
@@ -69,10 +65,18 @@ def run(m_id):
             batch_x = batch_x[:, perms, :, :]
             batch_y = batch_y[:, perms, :, :]
 
-        loss_val, _ = sess.run([loss, grad_update], feed_dict={x: batch_x, y: batch_y})
-        print('steps = {0} | loss = {1:.3f} | time {2:.3f}'.format(str(i).zfill(3),
-                                                                   loss_val,
-                                                                   time() - st))
+        mo, loss_val, _ = sess.run([model_out, loss, grad_update], feed_dict={x: batch_x,
+                                                                              y: batch_y})
+
+        rl = np.mean(np.square(mo[np.where(batch_y == 1)] - batch_y[np.where(batch_y == 1)]))
+        print('steps = {0} | loss = {1:.3f} | time {2:.3f} | rl = {3:.3f}'.format(str(i).zfill(3),
+                                                                                  loss_val,
+                                                                                  time() - st, rl))
+
+        if i % 50 == 0:
+            pred = sess.run(model_out, feed_dict={x: batch_x})[0].squeeze()
+            print(pred.shape)
+            visualise_mat(pred)
 
 
 if __name__ == '__main__':
--- md_lstm.py
+++ md_lstm.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 from tensorflow.contrib.rnn import RNNCell, LSTMStateTuple
-# from tensorflow.contrib.rnn.python.ops.core_rnn_cell_impl import _linear
-from tensorflow.python.ops.rnn_cell_impl import _linear
+from tensorflow.contrib.rnn.python.ops.core_rnn_cell import _linear
 
 
 def ln(tensor, scope=None, epsilon=1e-5):
