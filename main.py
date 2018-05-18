070b3ec4b14a9674adaea5b360bb731829910c27
philipperemy <premy@cogent.co.jp>
2018-05-18 10:31:52 +0900

    refactoring now. To test.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import logging
 import numpy as np
 import tensorflow.contrib.slim as slim
 
-from data_gen_corr_fields import next_batch, visualise_mat, find_target_for_tensor
+from data_gen_corr_fields import next_batch, visualise_mat, get_relevant_prediction_index
 from md_lstm import *
 from md_lstm import standard_lstm
 
@@ -69,26 +69,25 @@ def run(model_type='md_lstm'):
 
         mo, loss_val, _ = sess.run([model_out, loss, grad_update], feed_dict={x: batch_x, y: batch_y})
 
-        target_of_interest = find_target_for_tensor(batch_y)
-
         """
         ____________
         |          |
         |          |
         |     x    |
-        |      x <----- extract this prediction. Real loss is only computed for this value.
+        |      x <----- extract this prediction. Relevant loss is only computed for this value.
         |__________|    we don't care about the rest (even though the model is trained on all values
-                        for simplicity). A standard LSTM should have a very high value for real loss
+                        for simplicity). A standard LSTM should have a very high value for relevant loss
                         whereas a MD LSTM (which can see all the TOP LEFT corner) should perform well. 
         """
 
         # extract the predictions for the second x
-        ground_truth = np.array([batch_y[i, x, y, 0] for (i, (y, x)) in enumerate(target_of_interest)])
-        model_pred = np.array([mo[i, x, y, 0] for (i, (y, x)) in enumerate(target_of_interest)])
-        real_loss = np.mean(np.square(ground_truth - model_pred))
+        relevant_pred_index = get_relevant_prediction_index(batch_y)
+        true_rel = np.array([batch_y[i, x, y, 0] for (i, (y, x)) in enumerate(relevant_pred_index)])
+        pred_rel = np.array([mo[i, x, y, 0] for (i, (y, x)) in enumerate(relevant_pred_index)])
+        relevant_loss = np.mean(np.square(true_rel - pred_rel))
 
-        format_str = 'steps = {0} | overall loss = {1:.3f} | time {2:.3f} | real loss = {3:.3f}'
-        logger.info(format_str.format(str(i).zfill(3), loss_val, time() - st, real_loss))
+        format_str = 'steps = {0} | overall loss = {1:.3f} | time {2:.3f} | relevant loss = {3:.3f}'
+        logger.info(format_str.format(str(i).zfill(3), loss_val, time() - st, relevant_loss))
 
         if i % 500 == 0:
             visualise_mat(sess.run(model_out, feed_dict={x: batch_x})[0].squeeze())
